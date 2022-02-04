26c072d415f9e60bbc8a39cbb2739132806063ba
Annop Chawwalitsitthikun <sourcecode@annopnod.com>
2022-02-04 13:48:32 +0700

    Update Prop

--- RaylibGUIPlus/GUITextBox.h
+++ RaylibGUIPlus/GUITextBox.h
@@ -1,30 +1,38 @@
 #ifndef GUITextBox_H
 #define GUITextBox_H
 #include "lib/raylib.h"
-#include "Alignment.h"
+#include "Properties/Alignment.h"
 #include <string>
 #include <iostream>
+#include <functional>
+ 
 namespace RaylibGUIPlus {
+
+typedef enum TextBoxType { Text, Password } TextBoxType;
+
 	class GUITextBox
 {
 public:
 	
 	GUITextBox();
 	GUITextBox(Rectangle rect);
-
+	 
+	void AdjustmentHeight();
 	Color BackgroundColor = WHITE;
 	Color BorderColor = BLACK;
 	Color TextColor = BLACK;
 	int BorderSize = 1;
 	Font Font = GetFontDefault();
-	int MaxLength = 12;
+	int MaxLength = 25;
 	Rectangle Position;
 	void Render();
 	int ReadOnly = false;
 	RaylibGUIPlus::Alignment TextAlignment = Alignment::Left;
 	std::string Value = "";
+	TextBoxType TextBoxType= TextBoxType::Text;
 private:
 	bool focus = false;
+	std::function<void(GUITextBox*)> callback;
 };
 	GUITextBox::GUITextBox() {
 
@@ -33,23 +41,27 @@ private:
 	{
 		this->Position = rect;
 	}
-
+	 
+	void GUITextBox::AdjustmentHeight() {		 
+		this->Position.height = this->Font.baseSize ;
+		 	
+	}
 	void GUITextBox::Render()
 	{
+		DrawRectangle(
+			this->Position.x- this->BorderSize,
+			this->Position.y- this->BorderSize,
+			this->Position.width+ (this->BorderSize*2),
+			this->Position.height+ (this->BorderSize*2),
+			this->BorderColor);
+
 		DrawRectangle(
 			this->Position.x,
 			this->Position.y,
 			this->Position.width,
 			this->Position.height,
-			this->BorderColor);
-
-		DrawRectangle(
-			this->Position.x + this->BorderSize,
-			this->Position.y + this->BorderSize,
-			this->Position.width - (this->BorderSize * 2),
-			this->Position.height - (this->BorderSize * 2),
 			this->BackgroundColor);
-		Vector2 mousePoint = GetMousePosition();
+			Vector2 mousePoint = GetMousePosition();
 
 		if (CheckCollisionPointRec(mousePoint, this->Position) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
 			this->focus = true;
@@ -78,52 +90,55 @@ private:
 			else if (IsKeyPressed(KEY_BACKSPACE) && this->Value.length() > 0) {
 				this->Value.pop_back();
 			}
+			if (IsKeyPressed(KEY_ENTER)) {
+			 
+			}
 		}
-		std::string clonetext = this->Value;
+		std::string clonetext = "";
+		if (this->TextBoxType == TextBoxType::Text) {
+			clonetext = this->Value;
+		} else if (this->TextBoxType == TextBoxType::Password) {
+			 clonetext.append(this->Value.length(), '*');
+		}
+		
 		Vector2 textpos;
-		textpos = MeasureTextEx(this->Font, clonetext.c_str(), this->Font.baseSize, 1);
+		textpos = MeasureTextEx(this->Font, clonetext.c_str(), this->Font.baseSize, 0);
 
 		if (textpos.x > this->Position.width) {
-
 			do {
 
 				clonetext.erase(0, 1);
 				textpos = MeasureTextEx(this->Font, clonetext.c_str(), this->Font.baseSize, 1);
 			} while (textpos.x > this->Position.width);
 		}
-
-
-
 		Vector2 startdraw;
-
-
 		if (this->TextAlignment == Alignment::Center) {
 
 			float posx;
 			posx = (this->Position.width / 2) - (textpos.x / 2);
 
-			posx = posx + this->Position.x + this->BorderSize;
+			posx = posx + this->Position.x ;
 
 			startdraw = (Vector2)
 			{
-			posx , this->Position.y + this->BorderSize
+			posx , this->Position.y 
 			};
 		}
 		else if (this->TextAlignment == Alignment::Right) {
 			float posx;
 			posx = (this->Position.width) - (textpos.x);
 
-			posx = posx + this->Position.x + this->BorderSize;
+			posx = posx + this->Position.x ;
 
 			startdraw = (Vector2)
 			{
-			posx , this->Position.y + this->BorderSize
+			posx , this->Position.y  
 			};
 		}
 		else {
 			startdraw = (Vector2)
 			{
-			this->Position.x + this->BorderSize, this->Position.y + this->BorderSize
+			this->Position.x  , this->Position.y 
 			};
 		}
 		 
@@ -136,7 +151,7 @@ private:
 			if (this->TextAlignment == Alignment::Right) {
 				startdraw.x = this->Position.x +this->Position.width;
 			}
-			DrawLineEx(startdraw, (Vector2) { startdraw.x, startdraw.y + textpos.y }, this->BorderSize*2, this->TextColor);
+			DrawLineEx(startdraw, (Vector2) { startdraw.x, startdraw.y + textpos.y }, this->Font.baseSize*0.1, this->TextColor);
 			
 		}
 
--- RaylibGUIPlus/Test.cpp
+++ RaylibGUIPlus/Test.cpp
@@ -1,6 +1,5 @@
  
 #include "lib/raylib.h"
-#include "Alignment.h"
 #include "GUITextBox.h"
 #include <iostream>
 
@@ -9,23 +8,25 @@ int main()
     const int screenWidth = 1024;
     const int screenHeight = 768;
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    InitWindow(screenWidth, screenHeight, "raylib=>RaylibGUIPlus Test");
     RaylibGUIPlus::GUITextBox textbox1= RaylibGUIPlus::GUITextBox(Rectangle{ 20,20,400,100 });
     RaylibGUIPlus::GUITextBox textbox2 = RaylibGUIPlus::GUITextBox(Rectangle{ 100,100,400,100 });
     
     
     textbox1.Font = LoadFontEx("../font/opensans/OpenSans-Regular.ttf", 35, 0, 0);
-    textbox1.TextAlignment = RaylibGUIPlus::Alignment::Right;
-
+    textbox1.TextAlignment = RaylibGUIPlus::Alignment::Left;
+    textbox1.AdjustmentHeight();
     textbox2.Font = LoadFontEx("../font/opensans/OpenSans-Regular.ttf", 35, 0, 0);
     textbox2.TextAlignment = RaylibGUIPlus::Alignment::Center;
     textbox1.BorderSize = 20;
+    textbox2.AdjustmentHeight();
+    textbox1.TextBoxType = RaylibGUIPlus::TextBoxType::Password;
+    
     SetTargetFPS(60);              
     while (!WindowShouldClose())    
     {
        
         BeginDrawing();
-
         ClearBackground(RAYWHITE);
         textbox1.Render();
         textbox2.Render();
